0992092e967a2869f45970bbc6eb8435682b0513
moraesc <cmoraes1955@gmail.com>
2023-12-16 16:49:51 -0500

    first commit

---
+++ app/data.tsx
@@ -0,0 +1,35 @@
+export const DATA = [
+  {
+    title: "Mourning",
+    artist: "Post Malone",
+    coverArt: "/./static/images/Austin.png",
+    audio: "./static/Mourning.mp3",
+    gradient: `linear-gradient(
+      to right,
+      rgb(219, 234, 254),
+      rgb(147, 197, 253),
+      rgb(59, 130, 246)
+    )`,
+  },
+  {
+    title: "Love Lost",
+    artist: "Mac Miller",
+    coverArt: "/./static/images/love-lost.png",
+    audio: "./static/love-lost.mp3",
+    gradient: `linear-gradient(to right, rgb(244, 63, 94), rgb(248, 113, 113), rgb(239, 68, 68))`,
+  },
+  {
+    title: "Think Fast",
+    artist: "Dominic Fike (feat. Weezer)",
+    coverArt: "/./static/images/think-fast.png",
+    audio: "./static/think-fast.mp3",
+    gradient: `linear-gradient(to right, rgb(248, 113, 113), rgb(209, 213, 219), rgb(59, 130, 246))`,
+  },
+  {
+    title: "Stick Season",
+    artist: "Noah Kahan",
+    coverArt: "/./static/images/stick-season.png",
+    audio: "./static/stick-season.mp3",
+    gradient: `linear-gradient(to right, rgb(187, 247, 208), rgb(34, 197, 94))`,
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,215 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import useSound from "use-sound"; // for handling the sound
+import { useCallback, useEffect, useRef, useState } from "react";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
 import { IconContext } from "react-icons"; // for customazing the icons
 import clsx from "clsx";
+import "./globals.css";
+import Image from "next/image";
+import { DATA } from "./data";
+import { useMediaQuery } from "usehooks-ts";
 
 export default function Home() {
+  const tablet = useMediaQuery("(min-width: 768px)");
+
+  const [trackProgress, setTrackProgress] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [play, { pause, duration, sound }] = useSound("./static/sample.mp3");
+
+  const [position, setPosition] = useState();
+
+  const cdRef = useRef<HTMLImageElement>();
+
+  console.log("cdRef: ", cdRef.current?.style.getPropertyValue("rotate"));
+
+  const [count, setCount] = useState(0);
+
+  const { title, artist, coverArt, audio, gradient } = DATA[count];
+
+  const audioRef = useRef<HTMLAudioElement>();
+  const intervalRef = useRef();
+  const isReady = useRef(false);
+
+  useEffect(() => {
+    audioRef.current?.pause();
+
+    audioRef.current = new Audio(audio);
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current && !isPlaying) {
+      audioRef.current.play();
+      // setIsPlaying(true);
+      startTimer();
+    } else {
+      // Set the isReady ref as true for the next pass
+      isReady.current = true;
+    }
+  }, [count]);
 
   const playingButton = () => {
     if (isPlaying) {
-      pause(); // this will pause the audio
+      audioRef.current?.pause(); // this will pause the audio
       setIsPlaying(false);
     } else {
-      play(); // this will play the audio
+      audioRef.current?.play(); // this will play the audio
       setIsPlaying(true);
     }
   };
 
-  const [currTime, setCurrTime] = useState({
-    min: "",
-    sec: "",
-  }); // current position of the audio in minutes and seconds
+  const [rotation, setRotation] = useState(0);
 
-  const [seconds, setSeconds] = useState(); // current position of the audio in seconds
+  // useEffect(() => {
+  //   if (isPlaying) {
+  //     document.getElementById('cd')?.style.transform = `rotate(${rotation}deg)`
+  //   }
+  // }, [isPlaying])
 
-  const sec = duration / 1000;
-  const min = Math.floor(sec / 60);
-  const secRemain = Math.floor(sec % 60);
-  const time = {
-    min: min,
-    sec: secRemain,
+  const rotateForward = () => {
+    const newAudioNumber = count + 1;
+    setCount(newAudioNumber);
+    console.log("exists? ", document.getElementById("cd"));
+    document.getElementById("cd").className = clsx(
+      "rotate-forward",
+      isPlaying && "spinRecord"
+    );
+  };
+
+  const rotateBackward = () => {
+    const newAudioNumber = count - 1;
+    setCount(newAudioNumber);
+    console.log("exists? ", document.getElementById("cd"));
+    document.getElementById("cd").className = clsx(
+      "rotate-backward",
+      isPlaying && "spinRecord"
+    );
+  };
+
+  const startTimer = useCallback(() => {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current?.ended) {
+        rotateForward();
+      } else {
+        console.log("in else: ", audioRef.current);
+        if (!audioRef.current) return;
+        setTrackProgress(audioRef.current.currentTime);
+      }
+    }, [1000]);
+  }, [rotateForward]);
+
+  const onScrub = (value) => {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+    if (!audioRef.current) return;
+    audioRef.current.currentTime = value;
+    setTrackProgress(audioRef.current?.currentTime);
+  };
+
+  const onScrubEnd = () => {
+    // If not already playing, start
+    if (!isPlaying) {
+      setIsPlaying(true);
+    }
+    startTimer();
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (sound) {
-        setSeconds(sound.seek([])); // setting the seconds state with the current state
-        const min = Math.floor(sound.seek([]) / 60);
-        const sec = Math.floor(sound.seek([]) % 60);
-        setCurrTime({
-          min: min.toString(),
-          sec: sec.toString(),
-        });
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [sound]);
+    if (isPlaying) {
+      audioRef.current?.play();
+      startTimer();
+    } else {
+      clearInterval(intervalRef.current);
+      audioRef.current?.pause();
+    }
+  }, [isPlaying, startTimer]);
+
+  const currentPercentage = audioRef.current?.duration
+    ? `${(trackProgress / audioRef.current.duration) * 100}%`
+    : "0%";
 
   return (
-    <div className="flex justify-center h-[100vh] items-center w-full margin-auto">
+    <div
+      className="flex justify-center h-[100vh] items-center w-full margin-auto"
+      style={{ background: gradient }}
+    >
       <div>
-        <div className="relative h-[500px] flex justify-center margin-auto">
-          <img
-            className={clsx(
-              "rounded-[50%] drop-shadow-lg",
-              isPlaying && "spinRecord"
-            )}
-            src="https://picsum.photos/500/500"
-            alt="record"
-          />
-          <div className="flex bg-[#e9d8a6] w-[75px] h-[75px] rounded-[100%] z-2 absolute top-[42%] left-[42%]"></div>
-        </div>
-        <div className="mt-24 w-[550px] flex justify-center">
-          <button className="playButton">
-            <IconContext.Provider value={{ size: "3em", color: "#0a9396" }}>
-              <BiSkipPrevious />
-            </IconContext.Provider>
-          </button>
-          {!isPlaying ? (
-            <button className="playButton" onClick={playingButton}>
-              <IconContext.Provider value={{ size: "3em", color: "#0a9396" }}>
-                <AiFillPlayCircle />
-              </IconContext.Provider>
-            </button>
-          ) : (
-            <button className="playButton" onClick={playingButton}>
-              <IconContext.Provider value={{ size: "3em", color: "#0a9396" }}>
-                <AiFillPauseCircle />
-              </IconContext.Provider>
-            </button>
-          )}
-          <button className="playButton">
-            <IconContext.Provider value={{ size: "3em", color: "#0a9396" }}>
-              <BiSkipNext />
-            </IconContext.Provider>
-          </button>
-        </div>
-        {/* <div>
-          <div className="time">
-            <p>
-              {currTime.min}:{currTime.sec}
-            </p>
-            <p>
-              {time.min}:{time.sec}
-            </p>
+        <div className="relative flex justify-center margin-auto relative w-[325px] md:w-[450px]  items-center">
+          <div className="rounded-r-[30px] rounded-l-[100px] bg-white w-[325px] h-[120px] md:w-[450px] md:h-[150px] shadow-lg flex flex-col gap-[40px]">
+            <div className="flex flex-col left-[150px] md:left-[190px] top-[15px] relative">
+              <p className="text-[14px] md:text-[16px] font-medium z-2">
+                {title}
+              </p>
+              <p className="text-[12px] md:text-[14px]">{artist}</p>
+            </div>
+            <audio src={audio} ref={audioRef ?? null} preload="metadata">
+              <source type="audio/mpeg" src={audio} />
+            </audio>
+            <input
+              type="range"
+              value={trackProgress}
+              step="1"
+              min="0"
+              max={audioRef.current ? audioRef.current.duration : `0`}
+              className="progress w-[162px] md:w-[230px] relative left-[150px] md:left-[190px] bottom-[16px] md:bottom-[10px]"
+              onChange={(e) => onScrub(e.target.value)}
+              onMouseUp={onScrubEnd}
+              onKeyUp={onScrubEnd}
+              style={{
+                background: `-webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #9dcdf7), color-stop(${currentPercentage}, #e4e4e4))`,
+              }}
+            />
+            <div className="flex left-[143px] md:left-[181px] justify-between w-[177px] md:w-[250px] relative bottom-[56px] md:bottom-[45px]">
+              <button className="playButton" onClick={rotateBackward}>
+                <IconContext.Provider
+                  value={{ size: tablet ? "36px" : "30px", color: "#232323" }}
+                >
+                  <BiSkipPrevious />
+                </IconContext.Provider>
+              </button>
+              {!isPlaying ? (
+                <button className="playButton" onClick={playingButton}>
+                  <IconContext.Provider
+                    value={{ size: tablet ? "36px" : "30px", color: "#232323" }}
+                  >
+                    <AiFillPlayCircle />
+                  </IconContext.Provider>
+                </button>
+              ) : (
+                <button className="playButton" onClick={playingButton}>
+                  <IconContext.Provider
+                    value={{ size: tablet ? "36px" : "30px", color: "#232323" }}
+                  >
+                    <AiFillPauseCircle />
+                  </IconContext.Provider>
+                </button>
+              )}
+              <button className="playButton" onClick={rotateForward}>
+                <IconContext.Provider
+                  value={{ size: tablet ? "36px" : "30px", color: "#232323" }}
+                >
+                  <BiSkipNext />
+                </IconContext.Provider>
+              </button>
+            </div>
           </div>
-          <input
-            type="range"
-            min="0"
-            max={duration / 1000}
-            // default="0"
-            value={seconds}
-            className="timeline"
-            onChange={(e) => {
-              sound.seek([e.target.value]);
-            }}
-          />
-        </div> */}
+          <div className="absolute w-[140px] md:w-[175px] left-0 z-100">
+            <Image
+              id="cd"
+              ref={cdRef}
+              className={clsx(
+                "rounded-[50%] drop-shadow-lg opacity-100",
+                isPlaying && "spinRecord"
+              )}
+              width={175}
+              height={175}
+              src={coverArt}
+              alt="record"
+            />
+            <div className="flex w-[12px] h-[12px] md:w-[14px] md:h-[14px] rounded-[100%] z-2 absolute top-[70px] left-[66px] md:top-[80px] md:left-[80px] bg-white" />
+          </div>
+        </div>
       </div>
     </div>
   );
